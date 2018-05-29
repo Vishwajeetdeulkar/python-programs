0f753443c9d6ecb24d6ec1d0ed2d7b657c6e5e2a
Vishwajeet Deulkar <vishwajeetd13@gmail.com>
2018-05-29 19:26:00 +0530

    Add files via upload

---
+++ chained_conditions.py
@@ -0,0 +1,10 @@
+print 'Simple chained condition'
+x=int(raw_input("Enter first value x for comparison:  "))
+y=int(raw_input("Enter second value y for comparison:  "))
+
+if x < y:
+ print 'x is less than y'
+elif x > y:
+ print 'x is greater than y'
+else:
+ print 'x and y are equal'
---
+++ comparison_operators.py
@@ -0,0 +1,18 @@
+print 'Simple comparision operations'
+x=int(raw_input("Enter first value x for comparison:  "))
+y=int(raw_input("Enter second value y for comparison:  "))
+
+if x != y :
+ print 'x is not equal to y'
+if x > y :
+ print 'x is greater than y'
+if x < y :
+ print 'x is less than y'
+if x >= y :
+ print 'x is greater than or equal to y'
+if x <= y :
+ print 'x is less than or equal to y'
+if x is y :
+ print 'x is same as y'
+if x is not y :
+ print 'x is not y' 
